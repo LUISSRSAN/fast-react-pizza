c2776d3d9a39a348d87f62da1cb4f6f72fca43d1
Luis Santiago <luissantiago@Luiss-Air.lan>
2024-02-21 08:35:04 -0500

    createBrowserRouter and RouterProvider implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Cart from "./features/cart/Cart";
+import Menu from "./features/menu/Menu";
+import Home from "./ui/Home";
+import CreateOrder from "/features/order/CreateOrder";
+import Order from "/features/order/Order";
 
+const router = createBrowserRouter([
+
+  {
+    path:"/",
+    element: <Home/>,
+  },
+  {
+    path:"/menu",
+    element:<Menu/>,
+  },{
+    path:'/cart',
+    element :<Cart/>
+  },
+  {
+    path:'order/new',
+    element : <CreateOrder/>,
+  },
+  {
+    path:'/order/:orderId',
+    element:<Order/>,
+  }
+])
 
 export default function App(){
-  const x = 23;
-  return <div>
-    Hello Vite!
+
+return (<div>
+  <RouterProvider router = {router}/>;
   </div>
+)
 }
